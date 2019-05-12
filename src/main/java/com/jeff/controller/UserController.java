ddbf6478e708d56c034d8ed090a4c75ac5c8b8a5
jiefu813 <jiefu813@163.com>
2019-05-13 00:14:56 +0800

    整合log4j收集日志，并根据级别输出到不同的文件

--- src/main/java/com/jeff/controller/UserController.java
+++ src/main/java/com/jeff/controller/UserController.java
@@ -2,6 +2,7 @@ package com.jeff.controller;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,8 @@ import com.jeff.service.UserService;
 
 @RestController
 public class UserController {
+	
+	private Logger logger = Logger.getLogger(this.getClass());
 
 	@Autowired
 	private UserService userService;
@@ -29,6 +32,13 @@ public class UserController {
 		List<User> userList = userService.getUserList();
 		PageInfo<User> page = new PageInfo<>(userList);
 		return page;
+	}
+	
+	@RequestMapping("log")
+	public String log() {
+		logger.info("测试基本信息");
+		logger.error("测试错误信息");
+		return "hello";
 	}
 
 }
